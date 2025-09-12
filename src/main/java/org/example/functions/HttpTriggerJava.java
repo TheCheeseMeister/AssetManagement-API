feb9b4e2a2759f3665bc6220167df78e6a458e8f
Elijah Thompson <ssomething511@gmail.com>
2025-09-11 23:24:45 -0400

    Added function for uploading image as blob. Accepts metadata as well in the form of Map<String, String>.

--- src/main/java/org/example/functions/HttpTriggerJava.java
+++ src/main/java/org/example/functions/HttpTriggerJava.java
@@ -1,9 +1,15 @@
 package org.example.functions;
 
+import java.io.ByteArrayInputStream;
 import java.util.*;
 import java.sql.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
+
+import com.azure.storage.blob.*;
+import com.azure.storage.blob.models.*;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -119,5 +125,85 @@ public class HttpTriggerJava {
                 .header("Content-Type", "application/json")
                 .body(results)
                 .build();
+    }
+
+    @FunctionName("UploadImage")
+    public HttpResponseMessage uploadImage(
+            @HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION)
+            HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context
+            ) {
+        context.getLogger().info("Processing image...");
+
+        try {
+            String body = request.getBody().orElse(null);
+            if (body == null || body.isEmpty()) {
+                return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                        .body("Empty request body.")
+                        .build();
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> jsonMap = mapper.readValue(body, Map.class);
+
+            // Extract image
+            String base64Image = (String) jsonMap.get("image");
+            byte[] imageBytes = Base64.getDecoder().decode(base64Image);
+
+            // Extract metadata
+            List<Map<String, Object>> metadataList = (List<Map<String, Object>>) jsonMap.get("metadata");
+            Map<String, String> metadataMap = new HashMap<>();
+
+            for (Map<String, Object> entry : metadataList) {
+                String key = (String) entry.get("key");
+                Object value = entry.get("value"); // left as object just in case values are parsed weird
+                metadataMap.put(key, value.toString());
+            }
+
+            context.getLogger().info("Uploading to blob storage...");
+
+            String connectStr = System.getenv("ConnectBlobStorage");
+            String containerName = "images";
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                    .connectionString(connectStr)
+                    .buildClient();
+
+            BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+            if (!containerClient.exists()) {
+                return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Container 'images' doesn't exist.")
+                        .build();
+            }
+
+            // Name Blob
+            String blobName = UUID.randomUUID().toString() + ".png";
+            BlobClient blobClient = containerClient.getBlobClient(blobName);
+
+            // Upload image
+            ByteArrayInputStream dataStream = new ByteArrayInputStream(imageBytes);
+            blobClient.upload(dataStream, imageBytes.length, true);
+
+            // Set metadata for Blob
+            if (!metadataMap.isEmpty()) {
+                blobClient.setMetadata(metadataMap);
+            }
+
+            context.getLogger().info("Uploaded blob: " + blobName + " with metadata: " + metadataMap);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "Upload successful");
+            response.put("blobName", blobName);
+            response.put("metadata", metadataMap);
+
+            return request.createResponseBuilder(HttpStatus.OK)
+                    .header("Content-Type", "application/json")
+                    .body(mapper.writeValueAsString(response))
+                    .build();
+        } catch (Exception e) {
+            context.getLogger().severe("Error: " + e.getMessage());
+
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error: " + e.getMessage()).build();
+        }
     }
 }
